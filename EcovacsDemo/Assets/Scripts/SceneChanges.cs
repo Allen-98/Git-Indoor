ee609f719380cbbd7e87e6b7d190d8dead2e2e86
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2022-02-24 16:47:45 +0800

    Scene merge
    make day scene and night scene together, update scripts about day and night scene change

--- EcovacsDemo/Assets/Scripts/SceneChanges.cs
+++ EcovacsDemo/Assets/Scripts/SceneChanges.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+
 
 public class SceneChanges : MonoBehaviour
 {
+    public GameObject dayLights;
+    public GameObject nightLights;
+
+    public GameObject dayFloor;
+    public GameObject nightFloor;
+
+    public bool isDay;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDay = true;
     }
 
     // Update is called once per frame
@@ -17,15 +27,30 @@ public class SceneChanges : MonoBehaviour
         
     }
 
-    public void ToLgihtOffScene()
+    public void SceneChange()
     {
-        SceneManager.LoadScene("LightOffScene");
+        if (isDay)
+        {
+            isDay = false;
+            dayFloor.SetActive(false);
+            dayLights.SetActive(false);
+            nightFloor.SetActive(true);
+            nightLights.SetActive(true);
+
+            RenderSettings.ambientLight = new Color(102/255f, 100 / 255f, 99 / 255f);
+        }
+        else
+        {
+            isDay = true;
+            dayFloor.SetActive(true);
+            dayLights.SetActive(true);
+            nightFloor.SetActive(false);
+            nightLights.SetActive(false);
+
+            RenderSettings.ambientLight = new Color(1, 1, 1);
+        }
     }
 
 
-    public void ToLightOnScene()
-    {
-        SceneManager.LoadScene("LightOnScene");
-    }
 
 }
