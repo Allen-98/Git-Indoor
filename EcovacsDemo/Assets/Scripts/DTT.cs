3a1b7221d6f414c712f704b50c7cd2841c75e652
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2022-05-12 17:24:01 +0800

    ROG update

--- EcovacsDemo/Assets/Scripts/DTT.cs
+++ EcovacsDemo/Assets/Scripts/DTT.cs
@@ -4,17 +4,81 @@ using UnityEngine;
 
 public class DTT : MonoBehaviour
 {
-    public GameObject main;
+    [Header("Game Objects")]
+    public GameObject window;
+    public GameObject yellowFW;
+
+
+
+    [Header("Materials")]
+    public Material mt_window;
+    public Material mt_redFW;
+    public Material mt_yellowFW;
+    public Material mt_blueFW;
+
+
+    [Header("Scripts")]
+    public SceneChanges sc;
+
+
+
+
+    private float windowValue;
+    private float yellowValue;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mt_window.SetFloat("_FrostIntensity", 1f);
+
+
+        mt_yellowFW.SetColor("_Color", new Color(1, 1, 1, 1));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        main.transform.Rotate(Vector3.up, 1);
+
+        //WindowRemove();
+        //YellowFWRemove();
+
     }
+
+    public void WindowRemove()
+    {
+        windowValue = mt_window.GetFloat("_FrostIntensity");
+
+        if (windowValue > 0)
+        {
+            mt_window.SetFloat("_FrostIntensity", windowValue - 0.01f);
+        }
+        else
+        {
+            window.SetActive(false);
+        }
+
+
+    }
+
+    public void YellowFWRemove()
+    {
+        yellowValue = mt_yellowFW.GetColor("_Color").a;
+
+        if (yellowValue > 0)
+        {
+            yellowValue -= 0.01f;
+
+            mt_yellowFW.SetColor("_Color", new Color(1, 1, 1, yellowValue));
+
+        }
+        else
+        {
+            yellowFW.SetActive(false);
+        }
+
+
+    }
+
 }
