9b8413215f8fde8c31c293e8bbb8289a385175d6
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2022-05-16 16:34:59 +0800

    ROG Update
    精简脚本 调整顺序

--- EcovacsDemo/Assets/Scripts/DTT.cs
+++ EcovacsDemo/Assets/Scripts/DTT.cs
@@ -16,7 +16,6 @@ public class DTT : MonoBehaviour
     public GameObject dayLights;
     public GameObject nightLights;
     public Light[] lights;
-    public GameObject pivot;
     public GameObject airbot;
     public GameObject blanket;
     public GameObject socks;
@@ -29,9 +28,11 @@ public class DTT : MonoBehaviour
     public Material mt_yellowFW;
     public Material mt_blueFW;
     public Material mt_blanket;
+    public Material mt_shoes;
+    public Material mt_socks;
 
-    
 
+    private float removeValue;
     private float windowValue;
     private float yellowValue;
     private float blueValue;
@@ -40,10 +41,10 @@ public class DTT : MonoBehaviour
 
     private float sceneColorValue;
     private float lightIntensity;
-    private float pivotValue;
     private float radius;
 
     private bool over;
+    private bool over1;
 
 
     // Start is called before the first frame update
@@ -56,10 +57,16 @@ public class DTT : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        SceneChange1();
-
+        SceneLightChange();
+        
         if (over)
+        {
+            ThingsRemove();
+        }
+
+
+
+        if (over1)
         {
             if (airbot.transform.position.x < 0.6)
             {
@@ -69,9 +76,13 @@ public class DTT : MonoBehaviour
             else
             {
                 radius = circle.target1Radius;
-                if (radius < 10)
+                if (radius < 7)
                 {
                     radius += 0.01f;
+                    circle.target1Radius = radius;
+                } else if (radius >= 7 && radius < 380)
+                {
+                    radius += 5f;
                     circle.target1Radius = radius;
                 }
             }
@@ -82,12 +93,12 @@ public class DTT : MonoBehaviour
     public void StartSettings()
     {
         over = false;
+        over1 = false;
 
         mt_window.SetFloat("_FrostIntensity", 1f);
         mt_yellowFW.SetColor("_Color", new Color(1, 1, 1, 1));
         mt_blueFW.SetColor("_Color", new Color(1, 1, 1, 1));
         mt_redFW.SetColor("_Color", new Color(1, 1, 1, 1));
-        mt_blanket.SetFloat("_AdvancedDissolveCutoutStandardClip", 0f);
 
         foreach (Light i in lights)
         {
@@ -98,114 +109,59 @@ public class DTT : MonoBehaviour
         RenderSettings.ambientLight = new Color(1, 1, 1);
         sceneColorValue = 1f;
 
-        pivot.transform.position = new Vector3(0, 3.3f, 0);
-        pivotValue = pivot.transform.position.y;
-
         airbot.transform.position = new Vector3(-1.5f, 0.03f, 0.3f);
     }
 
-    public void WindowRemove()
+    public void ThingsRemove()
     {
-        windowValue = mt_window.GetFloat("_FrostIntensity");
+        removeValue = mt_window.GetFloat("_FrostIntensity");
 
-        if (windowValue > 0)
+        if (removeValue > 0)
         {
-            mt_window.SetFloat("_FrostIntensity", windowValue - 0.003f);
+            removeValue -= 0.02f;
+
+            mt_window.SetFloat("_FrostIntensity", removeValue);
+            mt_yellowFW.SetColor("_Color", new Color(1, 1, 1, removeValue));
+            mt_blueFW.SetColor("_Color", new Color(1, 1, 1, removeValue));
+            mt_redFW.SetColor("_Color", new Color(1, 1, 1, removeValue));
+
         }
         else
         {
             window.SetActive(false);
-        }
-
-
-    }
-
-    public void YellowFWRemove()
-    {
-        yellowValue = mt_yellowFW.GetColor("_Color").a;
-
-        if (yellowValue > 0)
-        {
-            yellowValue -= 0.003f;
-
-            mt_yellowFW.SetColor("_Color", new Color(1, 1, 1, yellowValue));
-
-        }
-        else
-        {
             yellowFW.SetActive(false);
-        }
-
-
-    }
-
-    public void BlueFWRemove()
-    {
-        blueValue = mt_blueFW.GetColor("_Color").a;
-
-        if (blueValue > 0)
-        {
-            blueValue -= 0.003f;
-
-            mt_blueFW.SetColor("_Color", new Color(1, 1, 1, blueValue));
-
-        }
-        else
-        {
             blueFW.SetActive(false);
             blueFW2.SetActive(false);
-        }
-
-
-    }
-
-    public void RedFWRemove()
-    {
-        redValue = mt_redFW.GetColor("_Color").a;
-
-        if (redValue > 0)
-        {
-            redValue -= 0.003f;
-
-            mt_redFW.SetColor("_Color", new Color(1, 1, 1, redValue));
-
-        }
-        else
-        {
             redFW.SetActive(false);
             redFw2.SetActive(false);
+            over1 = true;
         }
 
-
     }
+
 
     public void SceneLightChange()
     {
+        dayLights.SetActive(false);
 
         if (sceneColorValue > 0.4)
         {
-            sceneColorValue -= 0.002f;
+            sceneColorValue -= 0.01f;
             RenderSettings.ambientLight = new Color(sceneColorValue, sceneColorValue, sceneColorValue);
 
         } 
-        else if (sceneColorValue < 0.99)
+        else
         {
             dayLights.SetActive(false);
-            nightLights.SetActive(true);
 
-            if (lightIntensity < 2.25)
+            if (lightIntensity < 2.4)
             {
-                lightIntensity += 0.005f;
+                lightIntensity += 0.03f;
                 foreach (Light i in lights)
-                {
+                { 
                     i.intensity = lightIntensity;
                 }
 
-            }
-
-            if (pivotValue > 0)
-            {
-                pivot.transform.position = new Vector3(0, pivotValue -= 0.01f, 0);
             }
             else
             {
@@ -216,31 +172,5 @@ public class DTT : MonoBehaviour
 
     }
 
-    public void BlanketRemove()
-    {
-        blanketValue = mt_blanket.GetFloat("_AdvancedDissolveCutoutStandardClip");
-
-        if (blanketValue < 1)
-        {
-            blanketValue += 0.003f;
-            mt_blanket.SetFloat("_AdvancedDissolveCutoutStandardClip", blanketValue);
-        }
-        else
-        {
-            blanket.SetActive(false);
-            shoes.SetActive(false);
-            socks.SetActive(false);
-        }
-    }
-
-    public void SceneChange1()
-    {
-        WindowRemove();
-        YellowFWRemove();
-        BlueFWRemove();
-        RedFWRemove();
-        BlanketRemove();
-        SceneLightChange();
-    }
 
 }
